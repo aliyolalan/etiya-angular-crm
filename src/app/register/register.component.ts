347a973a71dbc20882633dca605344e9060badbc
Ali Yolalan <55928985+aliyolalan@users.noreply.github.com>
2022-05-10 20:41:50 +0300

    Register validators is OK.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,19 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { faSignIn } from '@fortawesome/free-solid-svg-icons';
-
+import { HttpClient } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { faSignIn, faRegistered } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  faSignIn = faSignIn;
+  faRegistered = faRegistered;
 
-  faSignIn = faSignIn
+  constructor(
+    private formBuilder: FormBuilder,
+    private routerService: Router,
+    private httpService: HttpClient,
+    private toastrService: ToastrService
+  ) {}
 
-  constructor() { }
+  userForm = this.formBuilder.group({
+    userName: '',
+    userSurname: '',
+    userPhone: '',
+    userEmail: '',
+    userPassword: '',
+  });
 
+  // Validators in LifeCycle Method...
   ngOnInit(): void {
+    this.userForm = new FormGroup({
+      userName: new FormControl(this.userForm.value.userName, [
+        Validators.required,
+      ]),
+      userSurname: new FormControl(this.userForm.value.userSurname, [
+        Validators.required,
+      ]),
+      userPhone: new FormControl(this.userForm.value.userPhone, [
+        Validators.required,
+      ]),
+      userEmail: new FormControl(this.userForm.value.userEmail, [
+        Validators.required,
+        Validators.email,
+      ]),
+      userPassword: new FormControl(this.userForm.value.userPassword, [
+        Validators.required,
+      ]),
+    });
   }
 
+  // Get Methods For NgIf... Conditional Rendering...
+  get userName() {
+    return this.userForm.get('userName');
+  }
+
+  get userSurname() {
+    return this.userForm.get('userSurname');
+  }
+
+  get userPhone() {
+    return this.userForm.get('userPhone');
+  }
+
+  get userEmail() {
+    return this.userForm.get('userEmail');
+  }
+
+  get userPassword() {
+    return this.userForm.get('userPassword');
+  }
+
+  registerFunction() {}
 }
